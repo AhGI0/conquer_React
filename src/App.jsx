568a30ca5528209aa60032a04a3fffee4134e478
Ahmed Sayed <ahmedsaye23156@gmail.com>
2024-12-13 19:04:45 +0200

    i established the new concepts here

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Greating from './Greating'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
+  let names= ['ahmed','nada','momen','katty','reda','maged','sara'];   
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    {names.map((e,index)=>(
+      <Greating name={e[0].toUpperCase() + e.slice(1)} />
+    ))}
     </>
   )
 }
-
-export default App
---
+++ src/Greating.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Greating({name}) {
+  return (
+    <div>hello, {name}</div>
+  )
+}
